c1d484fcb986deabc5560a59b07c1d948ea63356
joewo <joe.wojniak@gmail.com>
2018-05-07 21:52:11 -0600

    Added editText initializations.  Removed KeyListener for now.  Replaced SparseArray with HashMap due to mixed data types- String key & Integer value.

--- app/src/main/java/blog/globalquality/dartsscorekeeper/MainActivity.java
+++ app/src/main/java/blog/globalquality/dartsscorekeeper/MainActivity.java
@@ -3,60 +3,100 @@ package blog.globalquality.dartsscorekeeper;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.util.SparseArray;
-import android.view.KeyEvent;
-import android.view.View;
 import android.widget.EditText;
-import android.widget.Toast;
+
+import java.util.HashMap;
 
 import static blog.globalquality.dartsscorekeeper.R.id.editHomeDart1;
+import static blog.globalquality.dartsscorekeeper.R.id.editHomeDart2;
+import static blog.globalquality.dartsscorekeeper.R.id.editHomeDart3;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText edittext;
+    EditText pointsHomeDart1;
+    EditText pointsHomeDart2;
+    EditText pointsHomeDart3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        addKeyListener();
+        pointsHomeDart1 = (EditText) findViewById(editHomeDart1);
+        String pointsHomeDart1Is = (String) pointsHomeDart1.getText().toString();
+
+        pointsHomeDart2 = (EditText) findViewById(editHomeDart2);
+        String pointsHomeDart2Is = (String) pointsHomeDart2.getText().toString();
+
+        pointsHomeDart3 = (EditText) findViewById(editHomeDart3);
+        String pointsHomeDart3Is = (String) pointsHomeDart3.getText().toString();
     }
 
-    private void addKeyListener() {
+    /**
+     * This builds a HashMap with the dartboard segment points.
+     * This allows the throwScore method to lookup the number of points to assign to each shot.
+     * Segments 1-20, 25 and 50 can be converted to int using Integer.parseInt() and don't need to be included.
+     * The following segments are included:
+     *          Doubles: D1-D20, point value 2 - 40;
+     *          Trebles: T1-T20, point value 3 - 60;
+     *          Outer Bullseye: B or OB = 25;
+     *          Inner Bullseye (aka double Bullseye): IB, DB or D25 = 50;
+     */
+    public class Class {
 
-        // get edittext component
-        edittext = (EditText) findViewById(R.id.editHomeDart3);
+        HashMap<String, Integer> dartboardSegments = new HashMap<String, Integer>();
 
-        // add a keylistener to get entered values for Home Player's shots 1-3
-        // code stub modified from https://www.mkyong.com/android/android-prompt-user-input-dialog-example/
-        edittext.setOnKeyListener(new OnKeyListener() {
-            public boolean onKey(View v, int keyCode, KeyEvent event) {
+        public Class() {
 
-                // if keydown and "enter" is pressed
-                if ((event.getAction() == KeyEvent.ACTION_DOWN)
-                        && (keyCode == KeyEvent.KEYCODE_ENTER)) {
-
-                    // get entered values from editText for Home Player's shots 1-3
-                    EditText pointsHomeDart1 = (EditText) findViewById(editHomeDart1);
-                    String pointsHomeDart1Is = (String) pointsHomeDart1.getText().toString();
-
-                    EditText pointsHomeDart2 = (EditText) findViewById(R.id.editHomeDart2);
-                    String pointsHomeDart2Is = (String) pointsHomeDart2.getText().toString();
-
-                    EditText pointsHomeDart3 = (EditText) findViewById(R.id.editHomeDart3);
-                    String pointsHomeDart3Is = (String) pointsHomeDart3.getText().toString();
-
-                    String throwScoreHome = throwScore(pointsHomeDart1Is, pointsHomeDart2Is, pointsHomeDart3Is);
-
-                    Log.v("MainActivity", "Home scored: " + throwScoreHome);
-
-                    return true;
-
-                }
-                }
-            }
-        });
+            //Doubles
+            dartboardSegments.put("D1", 2);
+            dartboardSegments.put("D2", 4);
+            dartboardSegments.put("D3", 6);
+            dartboardSegments.put("D4", 8);
+            dartboardSegments.put("D5", 10);
+            dartboardSegments.put("D6", 12);
+            dartboardSegments.put("D7", 14);
+            dartboardSegments.put("D8", 16);
+            dartboardSegments.put("D9", 18);
+            dartboardSegments.put("D10", 20);
+            dartboardSegments.put("D11", 22);
+            dartboardSegments.put("D12", 24);
+            dartboardSegments.put("D13", 26);
+            dartboardSegments.put("D14", 28);
+            dartboardSegments.put("D15", 30);
+            dartboardSegments.put("D16", 32);
+            dartboardSegments.put("D17", 34);
+            dartboardSegments.put("D18", 36);
+            dartboardSegments.put("D19", 38);
+            dartboardSegments.put("D20", 40);
+            //Trebles
+            dartboardSegments.put("T1", 3);
+            dartboardSegments.put("T2", 6);
+            dartboardSegments.put("T3", 9);
+            dartboardSegments.put("T4", 12);
+            dartboardSegments.put("T5", 15);
+            dartboardSegments.put("T6", 18);
+            dartboardSegments.put("T7", 21);
+            dartboardSegments.put("T8", 24);
+            dartboardSegments.put("T9", 27);
+            dartboardSegments.put("T10", 30);
+            dartboardSegments.put("T11", 33);
+            dartboardSegments.put("T12", 36);
+            dartboardSegments.put("T13", 39);
+            dartboardSegments.put("T14", 42);
+            dartboardSegments.put("T15", 45);
+            dartboardSegments.put("T16", 48);
+            dartboardSegments.put("T17", 51);
+            dartboardSegments.put("T18", 54);
+            dartboardSegments.put("T19", 57);
+            dartboardSegments.put("T20", 60);
+            //Bullseye - allowing for possible abbreviations
+            dartboardSegments.put("B", 25);
+            dartboardSegments.put("OB", 25);
+            dartboardSegments.put("IB", 50);
+            dartboardSegments.put("DB", 50);
+            dartboardSegments.put("D25", 50);
+        }
     }
 
     /**
@@ -67,9 +107,9 @@ public class MainActivity extends AppCompatActivity {
      * @param shot3 : the segment hit on the third dart shot
      */
 
-    private String throwScore(String shot1, String shot2, String shot3) {
+    public String throwScore(String shot1, String shot2, String shot3) {
 
-        int shotScore1 = 0; // initialize local variables
+        int shotScore1 = 0;
         int shotScore2 = 0;
         int shotScore3 = 0;
 
@@ -77,21 +117,22 @@ public class MainActivity extends AppCompatActivity {
         try {
             shotScore1 = Integer.parseInt(shot1);
         } catch (NumberFormatException e) {
-            //lookup points value in dartboardSegmentsSparseArray
+            //lookup points value in hashMap
+            shotScore1 = dartboardSegments.get(shot1);
         }
 
         // convert String to int; 2nd dart thrown (2nd shot)
         try {
             shotScore2 = Integer.parseInt(shot2);
         } catch (NumberFormatException e) {
-            //lookup points value in dartboardSegmentsSparseArray
+            //lookup points value in hashMap
         }
 
         // convert String to int; 3rd dart thrown (3rd shot)
         try {
             shotScore3 = Integer.parseInt(shot3);
         } catch (NumberFormatException e) {
-            //lookup points value in dartboardSegmentsSparseArray
+            //lookup points value in hashMap
         }
 
         Log.v("MainActivity", "shotScore1: " + shotScore1);
@@ -101,69 +142,5 @@ public class MainActivity extends AppCompatActivity {
         return Integer.toString(shotScore1 + shotScore2 + shotScore3);
 
     }
-    /**
-     * This builds a SparseArray with the dartboard segment points.
-     * This allows the throwScore method to lookup the number of points to assign to each shot.
-     * Segments 1-20, 25 and 50 can be converted to int using Integer.parseInt() and don't need to be included.
-     * The following segments are included:
-     *          Doubles: D1-D20, point value 2 - 40;
-     *          Trebles: T1-T20, point value 3 - 60;
-     *          Outer Bullseye: B or OB = 25;
-     *          Inner Bullseye (aka double Bullseye): IB, DB or D25 = 50;
-     */
-    public class dartboardSegments {
 
-    SparseArray<dartboardSegments> dartboardSegmentsSparseArray = new SparseArray<dartboardSegments>();
-
-        private void setDartboardSegmentsSparseArray() {
-        //Doubles
-        dartboardSegmentsSparseArray.put(int D1, 2);
-        dartboardSegmentsSparseArray.put(int D2,4);
-        dartboardSegmentsSparseArray.put(int D3,6);
-        dartboardSegmentsSparseArray.put(int D4,8);
-        dartboardSegmentsSparseArray.put(int D5,10);
-        dartboardSegmentsSparseArray.put(int D6,12);
-        dartboardSegmentsSparseArray.put(int D7,14);
-        dartboardSegmentsSparseArray.put(int D8,16);
-        dartboardSegmentsSparseArray.put(int D9,18);
-        dartboardSegmentsSparseArray.put(int D10,20);
-        dartboardSegmentsSparseArray.put(int D11,22);
-        dartboardSegmentsSparseArray.put(int D12,24);
-        dartboardSegmentsSparseArray.put(int D13,26);
-        dartboardSegmentsSparseArray.put(int D14,28);
-        dartboardSegmentsSparseArray.put(int D15,30);
-        dartboardSegmentsSparseArray.put(int D16,32);
-        dartboardSegmentsSparseArray.put(int D17,34);
-        dartboardSegmentsSparseArray.put(int D18,36);
-        dartboardSegmentsSparseArray.put(int D19,38);
-        dartboardSegmentsSparseArray.put(int D20,40);
-        //Trebles
-        dartboardSegmentsSparseArray.put(int T1,3);
-        dartboardSegmentsSparseArray.put(int T2,6);
-        dartboardSegmentsSparseArray.put(int T3,9);
-        dartboardSegmentsSparseArray.put(int T4,12);
-        dartboardSegmentsSparseArray.put(int T5,15);
-        dartboardSegmentsSparseArray.put(int T6,18);
-        dartboardSegmentsSparseArray.put(int T7,21);
-        dartboardSegmentsSparseArray.put(int T8,24);
-        dartboardSegmentsSparseArray.put(int T9,27);
-        dartboardSegmentsSparseArray.put(int T10,30);
-        dartboardSegmentsSparseArray.put(int T11,33);
-        dartboardSegmentsSparseArray.put(int T12,36);
-        dartboardSegmentsSparseArray.put(int T13,39);
-        dartboardSegmentsSparseArray.put(int T14,42);
-        dartboardSegmentsSparseArray.put(int T15,45);
-        dartboardSegmentsSparseArray.put(int T16,48);
-        dartboardSegmentsSparseArray.put(int T17,51);
-        dartboardSegmentsSparseArray.put(int T18,54);
-        dartboardSegmentsSparseArray.put(int T19,57);
-        dartboardSegmentsSparseArray.put(int T20,60);
-        //Bullseye - allowing for possible abbreviations
-        dartboardSegmentsSparseArray.put(int B,25);
-        dartboardSegmentsSparseArray.put(int OB,25);
-        dartboardSegmentsSparseArray.put(int IB,50);
-        dartboardSegmentsSparseArray.put(int DB,50);
-        dartboardSegmentsSparseArray.put(int D25,50);
-    }
-}
 }
